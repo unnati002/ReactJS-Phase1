872c97df8aeebd7e8d5ffae3f6a49b6e05099a6b
unnati002 <52846512+unnati002@users.noreply.github.com>
2019-07-21 12:06:23 +0530

    Add files via upload

---
+++ Day-8.js
@@ -0,0 +1,17 @@
+const a=setTimeout(()=>{
+    console.log("React is the best")},5000);
+
+
+const b=setInterval(()=>{
+    console.log("React is the best")},3000);
+
+const c=setInterval(()=>{
+    console.log("React is the best")},1000);
+clearInterval(b);
+setTimeout(()=>{
+    clearInterval(c)},10000
+);
+for(i=0;i<10;i++)
+{
+    console.log("React is the best");
+}
